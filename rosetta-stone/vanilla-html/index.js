af860708c54b236a0fd6a9ef1f2b6a90af294411
Curran <curran.kelleher@gmail.com>
2024-07-27 12:35:43 -0400

    Update react and vanilla examples with interaction

--- rosetta-stone/vanilla-html/index.js
+++ rosetta-stone/vanilla-html/index.js
@@ -1,27 +1,15 @@
-import { renderSVG } from "./renderSVG.js";
+import { main } from "./viz/index.js";
 
-const main = (container) => {
-  const svg = renderSVG(container);
+const container = document.getElementById("viz-container");
+let state = {};
 
-  const data = [
-    { x: 155, y: 382, r: 20, fill: "#D4089D" },
-    { x: 340, y: 238, r: 52, fill: "#FF0AAE" },
-    { x: 531, y: 59, r: 20, fill: "#00FF88" },
-    { x: 482, y: 275, r: 147, fill: "#7300FF" },
-    { x: 781, y: 303, r: 61, fill: "#0FFB33" },
-    { x: 668, y: 229, r: 64, fill: "#D400FF" },
-    { x: 316, y: 396, r: 85, fill: "#0FF0FF" },
-  ];
-
-  svg
-    .selectAll("circle")
-    .data(data)
-    .join("circle")
-    .attr("cx", (d) => d.x)
-    .attr("cy", (d) => d.y)
-    .attr("r", (d) => d.r)
-    .attr("fill", (d) => d.fill)
-    .attr("opacity", 708 / 1000);
+const setState = (next) => {
+  state = next(state);
+  render();
 };
 
-main(document.getElementById("viz-container"));
+const render = () => {
+  main(container, { state, setState });
+};
+
+render();

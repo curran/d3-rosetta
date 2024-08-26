7644650dbd3642f301cdd408a398412d48db177c
Curran Kelleher <68416+curran@users.noreply.github.com>
2024-08-26 09:58:00 -0400

    Fix #22

--- src/observeResize.js
+++ src/observeResize.js
@@ -1,11 +1,22 @@
-import { StateField } from './StateField';
+import { StateField } from 'd3-rosetta';
+
 export const observeResize = (container, options) => {
   const [dimensions, setDimensions] =
     StateField(options)('dimensions');
-  if (!dimensions) {
-    new ResizeObserver((entries) => {
-      setDimensions(entries[0].contentRect);
-    }).observe(container);
-  }
-  return dimensions;
+  if (dimensions) return dimensions;
+  const getDimensions = () => ({
+    width: container.clientWidth,
+    height: container.clientHeight,
+  });
+
+  let isFirstCall = true;
+  new ResizeObserver(() => {
+    if (isFirstCall) {
+      isFirstCall = false;
+      return;
+    }
+    setDimensions(getDimensions());
+  }).observe(container);
+
+  return getDimensions();
 };

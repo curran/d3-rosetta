7e58ed00c7b8bcb0e2112021c02fd87c64c10181
Curran <curran.kelleher@gmail.com>
2025-06-15 22:44:42 -0400

    Fix index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 export { one } from './one.js';
-export { setter } from './setter.js';
 export { createMemoize } from './createMemoize.js';
 export { unidirectionalDataFlow } from './unidirectionalDataFlow.js';
 export { createStateField } from './createStateField.js';
